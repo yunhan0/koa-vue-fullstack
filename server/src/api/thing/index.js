093945c320ce6fbaf32c20fa4ecb3452c69f930e
yunhan0 <yunhn.lee@gmail.com>
2018-02-01 12:34:19 +1100

    async await with try catch

--- server/src/api/thing/index.js
+++ server/src/api/thing/index.js
@@ -15,29 +15,52 @@ let router = new Router({
 });
 
 router
+    // index
     .get('/', async (ctx, next) => {
-        // index
-        ctx.body = await Thing.find({}, 'name');
+        try {
+            ctx.body = await Thing.find({}, 'name');
+        } catch(err) {
+            ctx.throw(500, err);
+        }
     })
 
+    // create
     .post('/', async (ctx, next) => {
-        // create
-        ctx.body = await Thing.create(ctx.request.body);
+        try {
+            let newThing = await Thing.create(ctx.request.body);
+            ctx.status = 201; // Status code 201 : created
+            ctx.body = newThing;
+        } catch(err) {
+            ctx.throw(500, err);
+        }
     })
 
+    // get single item
     .get('/:id', async (ctx, next) => {
-        // show
-        ctx.body = await Thing.find({_id: ctx.params.id});
+        try {
+            ctx.body = await Thing.find({_id: ctx.params.id});
+        } catch(err) {
+// TODO: handle not found err.
+            ctx.throw(500, err);
+        }
     })
 
+    // update
     .put('/:id', (ctx, next) => {
-        // update
-        ctx.body = 'Thing update!';
+        try {
+            ctx.body = 'Thing update!';
+        } catch(err) {
+            ctx.throw(500, err);
+        }
     })
 
+    // delete
     .delete('/:id', async (ctx, next) => {
-        // delete
-        ctx.body = await Thing.remove({_id: ctx.params.id});
+        try {
+            ctx.body = await Thing.remove({_id: ctx.params.id});
+        } catch(err) {
+            ctx.throw(500, err);
+        }
     });
 
 module.exports = router;
