7f51d06a2aac735fb15f9aa153be9df4d97a437b
yunhan0 <yunhn.lee@gmail.com>
2021-01-11 16:42:29 +1100

    add readiness check in kubenetes pod and update database seeding

--- server/src/config/seed.js
+++ server/src/config/seed.js
@@ -7,25 +7,34 @@ const Thing = require('../api/thing/thing.model')
 const User = require('../api/user/user.model')
 
 const seeding = async() => {
-  await Thing.collection.deleteMany()
-  await Thing.create(
-    { name: 'Our first Koa and Node app', info: 'Lightweight server' },
-    { name: 'Mongo is here', info: 'We use mongodb to store data!' }
-  )
-  await User.collection.deleteMany()
-  await User.create({
-    name: 'tester',
-    email: 'test@example.com',
-    password: 'helloworld',
-    role: 'user'
-  })
-  await User.create({
-    name: 'admin',
-    email: 'admin@example.com',
-    password: 'admini',
-    role: 'admin'
-  })
-  console.log('Finish populating database')
+  let numberOfThings = await Thing.countDocuments()
+    // populate initial things if nothing is in the database
+  if (numberOfThings == 0) {
+    await Thing.create({
+      name: 'Our first Koa and Node app', info: 'Lightweight server'
+    }, {
+      name: 'Mongo is here', info: 'We use mongodb to store data!'
+    })
+    console.log('Finish populating database - things')
+  }
+
+  let numberOfUsers = await User.countDocuments()
+  // populate initial users if no user is in the database
+  if (numberOfUsers == 0) {
+    await User.create({
+      name: 'tester',
+      email: 'test@example.com',
+      password: 'helloworld',
+      role: 'user'
+    })
+    await User.create({
+      name: 'admin',
+      email: 'admin@example.com',
+      password: '123456',
+      role: 'admin'
+    })
+    console.log('Finish populating database - users')
+  }
 }
 
 seeding()
